4aeaa91b04184b110fe6dbbc1306d2935280adfb
Michael <762148+michael-folc@users.noreply.github.com>
2024-05-29 14:19:53 +0200

    Add projects for Discord

---
+++ src/Discord/Console/Program.cs
@@ -0,0 +1,13 @@
+﻿// SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: COPYRIGHT Lois & Clark Fanfiction Tooling
+
+using System;
+
+namespace LCFanfic.KerthAwardsCeremonyLog.Discord.Console;
+
+class Program
+{
+  static void Main (string[] args)
+  {
+  }
+}
